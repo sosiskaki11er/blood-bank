824bea57176caf5ccd0e894c5410bdf1d7043d4a
Abdulaziz <zakabduf@gmail.com>
2023-12-17 02:40:19 +0500

    fix: updated socket

--- socket/socket_server.c
+++ socket/socket_server.c
@@ -8,26 +8,36 @@
 #define PORT 8080
 #define MAX_CLIENTS 5
 
-struct ClientInfo {
+struct ClientInfo
+{
     char ip[INET_ADDRSTRLEN];
     int port;
     int client_socket;
     char client_type; // 'f' for front end, 'b' for back end
 };
 
+struct FrontEndUser
+{
+    int client_socket;
+    pthread_t thread;
+};
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+int b_user_socket = -1;
+struct FrontEndUser f_users[MAX_CLIENTS];
+
 void *handle_client(void *arg);
+void *handle_front_end_user(void *arg);
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; // Declare the mutex
-int b_user_socket = -1; // Global variable to store the back-end user socket
-int f_user_socket = -1; // Global variable to store the front-end user socket
-
-int main() {
+int main()
+{
     int server_fd;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
 
     // Create a socket
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
@@ -37,13 +47,15 @@ int main() {
     address.sin_port = htons(PORT);
 
     // Bind the socket to the port
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
         perror("Binding failed");
         exit(EXIT_FAILURE);
     }
 
     // Listen for incoming connections
-    if (listen(server_fd, 3) < 0) {
+    if (listen(server_fd, 3) < 0)
+    {
         perror("Listen failed");
         exit(EXIT_FAILURE);
     }
@@ -51,14 +63,16 @@ int main() {
     printf("Server listening on port %d...\n", PORT);
 
     pthread_t threads[MAX_CLIENTS];
-    struct ClientInfo client_info[MAX_CLIENTS]; // Use an array to store client information
+    struct ClientInfo client_info[MAX_CLIENTS];
     int thread_index = 0;
 
-    while (1) {
+    while (1)
+    {
         int new_socket;
 
         // Accept incoming connection
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+        {
             perror("Accept failed");
             exit(EXIT_FAILURE);
         }
@@ -70,7 +84,8 @@ int main() {
 
         // Receive the client type ('f' or 'b')
         char client_type;
-        if (read(new_socket, &client_type, sizeof(client_type)) <= 0) {
+        if (read(new_socket, &client_type, sizeof(client_type)) <= 0)
+        {
             perror("Failed to receive client type");
             close(new_socket);
             continue;
@@ -80,85 +95,125 @@ int main() {
         printf("User connected from %s:%d as type %c.\n", new_client.ip, new_client.port, new_client.client_type);
 
         // Check if a back-end user is already connected
-        if (new_client.client_type == 'b' && b_user_socket != -1) {
+        if (new_client.client_type == 'b' && b_user_socket != -1)
+        {
             printf("Another back-end user tried to connect. Rejecting the connection.\n");
             close(new_socket);
             continue;
         }
 
         // Create a new thread for each client
-        if (thread_index < MAX_CLIENTS) {
+        if (thread_index < MAX_CLIENTS)
+        {
             client_info[thread_index] = new_client;
-            if (pthread_create(&threads[thread_index], NULL, handle_client, &client_info[thread_index]) != 0) {
-                perror("Thread creation failed");
+            if (new_client.client_type == 'f')
+            {
+                // If a front-end user connects, handle communication in a separate thread
+                f_users[thread_index].client_socket = new_socket;
+                if (pthread_create(&f_users[thread_index].thread, NULL, handle_front_end_user, &f_users[thread_index]) != 0)
+                {
+                    perror("Thread creation failed");
+                }
+            }
+            else
+            {
+                // If a back-end user connects, handle communication in the main thread
+                b_user_socket = new_client.client_socket;
+                if (pthread_create(&threads[thread_index], NULL, handle_client, &client_info[thread_index]) != 0)
+                {
+                    perror("Thread creation failed");
+                }
             }
             thread_index++;
-
-            if (new_client.client_type == 'b') {
-                // If a back-end user connects, check if there's an existing back-end user
-                pthread_mutex_lock(&mutex);
-                if (b_user_socket != -1) {
-                    // Disconnect the existing back-end user
-                    close(b_user_socket);
-                    b_user_socket = -1;
-                }
-                b_user_socket = new_client.client_socket;
-                pthread_mutex_unlock(&mutex);
-            } else if (new_client.client_type == 'f') {
-                // If a front-end user connects, set the f_user_socket
-                pthread_mutex_lock(&mutex);
-                f_user_socket = new_client.client_socket;
-                pthread_mutex_unlock(&mutex);
-            }
         }
     }
-
-    // The server will keep running and accepting new connections
 
     return 0;
 }
 
-void *handle_client(void *arg) {
+void *handle_client(void *arg)
+{
     struct ClientInfo *client_info = (struct ClientInfo *)arg;
     int client_socket = client_info->client_socket;
     char buffer[1024] = {0};
-
     // Receive data from the client
-    while (read(client_socket, buffer, 1024) > 0) {
+    while (read(client_socket, buffer, 1024) > 0){
         printf("Received from %s:%d (%c): %s\n", client_info->ip, client_info->port, client_info->client_type, buffer);
-
-        // Forward messages from 'f' clients to corresponding 'b' clients
-        if (client_info->client_type == 'f') {
-            pthread_mutex_lock(&mutex);
-            if (b_user_socket != -1) {
-                // Forward the message to the back-end user in the expected format
+        pthread_mutex_lock(&mutex);
+        if (client_info->client_type == 'f'){
+            // Forward messages from front-end user to the connected back-end user
+            if (b_user_socket != -1){
+                // Include the socket file descriptor of the front-end user in the message
                 char formatted_message[1024];
-                snprintf(formatted_message, sizeof(formatted_message), "front:%s", buffer);
+                snprintf(formatted_message, sizeof(formatted_message), "front:%d:%s", client_socket, buffer);
                 write(b_user_socket, formatted_message, strlen(formatted_message));
+                printf("Forwarded to back end user (socket %d): %s\n", client_socket, formatted_message);
             }
-            pthread_mutex_unlock(&mutex);
-        } else if (client_info->client_type == 'b') {
-            // Reply to the front end user in the expected format
-            pthread_mutex_lock(&mutex);
-            if (f_user_socket != -1) {
-                write(f_user_socket, buffer, strlen(buffer));
-            }
-            pthread_mutex_unlock(&mutex);
         }
-
+        else if (client_info->client_type == 'b'){
+            // Check if the message starts with "back:" and extract the front-end socket ID and message
+            int front_socket;
+            char *message_start = strstr(buffer, "front:");
+            if (message_start != NULL && sscanf(message_start, "front:%d:%[^\n]", &front_socket, buffer) == 2){
+                // Reply to the specific front-end user in the expected format
+                char formatted_message[1024];
+                snprintf(formatted_message, sizeof(formatted_message), "back:%d:%s", front_socket, buffer);
+                write(front_socket, formatted_message, strlen(formatted_message));
+                printf("Replied to front end user (socket %d): %s\n", front_socket, formatted_message);
+            }
+        }
+        pthread_mutex_unlock(&mutex);
         memset(buffer, 0, sizeof(buffer));
     }
 
     printf("Client %s:%d (%c) disconnected.\n", client_info->ip, client_info->port, client_info->client_type);
     close(client_socket);
 
-    // Update the global socket variables when a client disconnects
+    // Update the global socket variable when a client disconnects
     pthread_mutex_lock(&mutex);
-    if (client_info->client_type == 'b' && b_user_socket == client_socket) {
+
+    if (client_info->client_type == 'b' && b_user_socket == client_socket)
+    {
         b_user_socket = -1;
-    } else if (client_info->client_type == 'f' && f_user_socket == client_socket) {
-        f_user_socket = -1;
     }
+
+    pthread_mutex_unlock(&mutex);
+
+    pthread_exit(NULL);
+}
+
+void *handle_front_end_user(void *arg)
+{
+    struct FrontEndUser *f_user = (struct FrontEndUser *)arg;
+    int client_socket = f_user->client_socket;
+    char buffer[1024] = {0};
+
+    // Receive data from the front-end user
+    while (read(client_socket, buffer, 1024) > 0)
+    {
+        printf("Received from front end user (socket %d): %s\n", client_socket, buffer);
+
+        // Forward messages from the front end user to the back end
+        pthread_mutex_lock(&mutex);
+        if (b_user_socket != -1)
+        {
+            // Forward the message to the back-end user in the expected format
+            char formatted_message[1024];
+            snprintf(formatted_message, sizeof(formatted_message), "front:%d:%s", client_socket, buffer);
+            printf("Forwarding to back end user: %s\n", formatted_message);
+            write(b_user_socket, formatted_message, strlen(formatted_message));
+        }
+        pthread_mutex_unlock(&mutex);
+
+        memset(buffer, 0, sizeof(buffer));
+    }
+
+    printf("Front end user (socket %d) disconnected.\n", client_socket);
+    close(client_socket);
+
+    // Update the global socket variable when a front-end user disconnects
+    pthread_mutex_lock(&mutex);
+    f_user->client_socket = -1;
     pthread_mutex_unlock(&mutex);
 
     pthread_exit(NULL);
